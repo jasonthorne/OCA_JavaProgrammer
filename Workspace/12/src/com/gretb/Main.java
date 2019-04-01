43d4ded420c1cfa0d9bccb8e6d2d78cf7e57668b
jasonthorne <jasonthorne@live.ie>
2019-04-01 15:33:29 +0100

    EOD push

--- Workspace/12/src/com/gretb/Main.java
+++ Workspace/12/src/com/gretb/Main.java
@@ -3,7 +3,18 @@ package com.gretb;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("bum");
+		
+		
+		int[]numArray = {45, 83, 990, 78};
+		
+		for(int i=0;i<numArray.length;i++) {
+			System.out.println("Number is: " + numArray[i]);
+		}
+		
+		
+		for(int j: numArray) {
+			System.out.println("number in enhanced for is: " + j);
+		}
 
 	}
 
