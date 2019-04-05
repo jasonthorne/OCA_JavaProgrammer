d29244920de6e240bd7575f69f9d6e7d8f889458
jasonthorne <jasonthorne@live.ie>
2019-04-05 12:30:34 +0100

    EOD push

--- Workspace/20/src/com/gretb/Dog.java
+++ Workspace/20/src/com/gretb/Dog.java
@@ -24,12 +24,58 @@ public class Dog {
 	
 	
 	/**
-	 * POLYMORPHISM
+	 * POLYMORPHISM (Overloading, and overriding)
+	 * A method can do different things depending on what calls it and what is sent to it.
+	 * Different animals like a fish, bird and dog all have a method called move()
+	 * how they move is different.
+	 * A fish swims
+	 * A bird flies
+	 * A dog walks
+	 * Same method, but different implementation
+	 *
+	 * 
+	 * 
+	 * OVERLOADING:
+	 * What uniquely identifies a method is NOT THE NAME. What uniquely identifies a method is its name + the list of parameter types
+	 * (int age, String name, Dog spot)
+	 * Also ORDER of parameters matters. return type doesn't matter, and isn't considered for overloading
 	 */
+	
 
-	void eat() {
+	void eat(double amount) {
 		System.out.println("");
 	}
+	
+	void eat(double amount, int num) {
+		System.out.println("");
+	}
+	
+	
+	void eat(int num, double amount) { //DIFFERENT FROM ABOVE!! due to parameter order 
+		
+	}
+	
+	
+	/*
+	void eat(double thisdoesntMatter) { //INVALID as constructor exists. (name of parameter is irrelevant!)
+		System.out.println("");
+	}
+	*/
+	
+	/*
+	int eat(double amount) { //INVALID as constructor exists. (return type is irrelevant!)
+		return amount;
+	}
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- Workspace/20/src/com/gretb/Main.java
+++ Workspace/20/src/com/gretb/Main.java
@@ -8,6 +8,12 @@ public class Main {
 		tibbles.eat();
 		
 		Dog spot = new Dog(4, 5.6, "Spot");
+		
+		//different method calls
+		spot.eat();
+		spot.eat(3.4);
+		spot.eat(3.5, 3);
+		spot.eat(3, 3.5);
 
 	}
 
