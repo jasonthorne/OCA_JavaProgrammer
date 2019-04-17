5be704c63260c0ec15cb4531fc5ab947532b250c
jasonthorne <jasonthorne@live.ie>
2019-04-17 14:57:23 +0100

    27 - 1

---
+++ Workspace/27/src/com/gretb/Main.java
@@ -0,0 +1,35 @@
+package com.gretb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Trainee terry = new Trainee(245, "Terry", 33);
+		Trainee bob = new Trainee(300, "Bob", 50);
+		Trainee jimmy = new Trainee(105, "Jimmy", 40);
+		
+		System.out.println(terry.name);
+		System.out.println(bob.name);
+		System.out.println(jimmy.name);
+		
+		//the same, as they all share this static (or CLASS) variable:
+		System.out.println(terry.courseCode);
+		System.out.println(bob.courseCode);
+		System.out.println(jimmy.courseCode);
+		
+		jimmy.courseCode = 4; //change this static var. (Accessing a static variable in a non static way. NOT recommended.) +++++++++
+		System.out.println("Bob's course code: " + bob.courseCode); //all other objects have new value
+		
+		//accessing static variable in a static way: Name of the class, followed by the static variable or method. This is the RIGHT way to access a static +++++++++
+		System.out.println("accessing static location of course: " + Trainee.location); 
+		
+		//accessing the static counter:
+		System.out.println("counter: " + Trainee.counter); //referenced by the Class, NOT an any instance of that class (eg: terry.counter)
+		
+		Trainee.riot(); //calling a static method in the RIGHT way. (Class name followed by method name).
+		
+		
+		
+	}
+
+}
---
+++ Workspace/27/src/com/gretb/Trainee.java
@@ -0,0 +1,50 @@
+package com.gretb;
+
+public class Trainee {
+	
+	int pps;
+	String name;
+	int age;
+	
+	/*
+	 Static is what is known as a class variable in that there is only ONE copy of the variable for the whole class. 
+	 As opposed to an instance where every object has a pps, name, age, etc.
+	 */
+	
+	//STATIC or CLASS variables:
+	static int courseCode = 2367; //all objects will share this same variable. If this is changed anywhere, it will change for all Trainee objects. ++++++++++++++++++
+	static String location = "Galway"; //All attendees are attending the course that is in Galway. 
+	
+	
+	static int counter = 0; //counts how many objects are being created
+
+	//constructor for Trainees
+	public Trainee(int pps, String name, int age) {
+		super();
+		this.pps = pps;
+		this.name = name;
+		this.age = age;
+		counter++;
+	}
+	
+	
+	//Static method +++++++++++++++++++++++++++++++++
+	/*
+	 Static method is the same as static variables in that these methods apply to the class and not the individual objects.
+	 The riot method below belongs to everyone in the class, you cant access individual attributes of a particular trainee
+	 */
+	static void riot() { //you can only access static things in this method!
+		
+		//You cant access instance vars/attributes of an individual trainee in a method that applies to every trainee. 
+		//System.out.println("age is " + age); //age can't be accessed here
+		System.out.println("courseCode is " + courseCode);
+		System.out.println("location is " + location);
+		
+		//How to access non static vars in a static method:
+		Trainee roger = new Trainee(234, "Roger", 34);
+		System.out.println(roger.age);
+		
+	}
+	
+
+}
