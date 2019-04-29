35c6930921020d8d5a33d78584040a135549e589
jasonthorne <jasonthorne@live.ie>
2019-04-29 12:29:51 +0100

    sec 1.1 - 4

--- Workspace/Section1.1/src/com/animal/Cow.java
+++ Workspace/Section1.1/src/com/animal/Cow.java
@@ -1,5 +1,18 @@
 package com.animal;
 
+import com.animal.mammal.Reptile; //this is importing ALL the classes in the com.animal.mammal package.
+
+/*
+ * If on imports, Cow class only has access to public classes in same package,
+ * so only has access to com.animal.
+ */
+
 public class Cow {
+	
+	Duck daffy = new Duck(); //Duck is a class in the same package
+	
+	Reptile ralf = new Reptile(); //Reptile is from imported class
+	
+	//Horse harry = 
 
 }
---
+++ Workspace/Section1.1/src/com/animal/mammal/Reptile.java
@@ -0,0 +1,24 @@
+package com.animal.mammal; //we are in com.animal.mammal
+
+import com.building.*; //import EVERYTHING from building package
+
+import com.animal.*; //import EVERYTHING from building package
+
+/*
+ * This is the Reptile.java file, so if we have a public class in this file, it has to be called Reptile. 
+ * Any other class of any other name CANT be public, as public class HAS TO be the same name ss the file name. 
+ */
+
+//This class doesn't HAVE to be public. It can be default (no keyword), but ONLY this class in this file can be public as it has the same name as the file. 
+public class Reptile { 
+
+}
+
+class Dinosaur{
+	
+}
+
+class Crocodile{
+	
+}
+
