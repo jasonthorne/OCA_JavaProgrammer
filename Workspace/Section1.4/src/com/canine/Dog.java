986fe60ebb39de08c8291648ad8188394d742aeb
jasonthorne <jasonthorne@live.ie>
2019-04-30 15:28:06 +0100

    EOD Push

--- Workspace/Section1.4/src/com/canine/Dog.java
+++ Workspace/Section1.4/src/com/canine/Dog.java
@@ -15,6 +15,18 @@ public class Dog extends Animal{
 		//System.out.println(weight); //has no access to weight, as Dog is in a different package to Animal. and weight is package level access.
 		System.out.println(height); //it has access to this as it's protected
 		
+		run(); //public
+		sleep(); //protected
+		
+		Dog spot = new Dog();
+		spot.sleep();
+		System.out.println(spot.name);
+		
+		Animal dAnimal = new Animal();
+		System.out.println("name of dAnimal is: " + dAnimal.name);
+		
+		//System.out.println(dAnimal.height);
+		
 	}
 
 }
