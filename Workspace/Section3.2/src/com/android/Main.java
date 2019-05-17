7c5ebd6db2948e9fe3c55a71e0f2e4b915d296c9
jasonthorne <jasonthorne@live.ie>
2019-05-17 11:55:10 +0100

    3.2 - 2

--- Workspace/Section3.2/src/com/android/Main.java
+++ Workspace/Section3.2/src/com/android/Main.java
@@ -3,7 +3,30 @@ package com.android;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//declare a var reference of type Person (ie not an instantiated object)
+		Person paul; 
+		
+		//an obj is only created when the 'new' keyword is used. This is person x
+		Person pauline = new Person(); //person x
+		
+		//assigning the paul reference to the pauline object. Now pauline and paul refer to the same person x.
+		paul=pauline;
+		
+		Person patsy = paul; //patsy, paul and pauline are all now referencing the same object.
+		
+		pauline = new Person(); //person y. pauline reference now points to a new object. Paul and patsy are now the ONLY 2 references pointing to person x.
+		
+		patsy = new Person(); //person z. 3 people created at this point. 
+		
+		paul = new Person(); //person n. Now that paul is dereferenced, noone is pointing to person x. So it's ready for garbage collection.
+		//4 people have been created, but only 3 can now be referenced.
+		
+		
+		pauline=paul; //pauline now refers to person n
+		paul=null; //paul now refers to nothing. (Remember Person n can still be reached through pauline).
+		
+		
 
 	}
 
--- Workspace/Section3.2/src/com/android/Person.java
+++ Workspace/Section3.2/src/com/android/Person.java
@@ -2,4 +2,21 @@ package com.android;
 
 public class Person {
 
+	static int counter = 0; //counts how many Person objects are created.
+	int age;
+	
+	//constructor
+	Person(){
+		System.out.println("Person created");
+		counter++;
+		System.out.println(counter + " people created");
+	}
+	
+	//overloaded constructor
+	Person(int age){
+		this.age=age;
+		System.out.println("Person created");
+		counter++;
+		System.out.println(counter + " people created");
+	}
 }
