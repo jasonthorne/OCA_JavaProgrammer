f8b1675753af73cb3b74724d61f042bbe3e98f49
jasonthorne <jasonthorne@live.ie>
2019-05-17 11:06:49 +0100

    3.2 - 1

--- Workspace/Section3.1/src/com/android/Phone.java
+++ Workspace/Section3.1/src/com/android/Phone.java
@@ -20,7 +20,7 @@ public class Phone {
 	
 	
 	void dialUp(int number) {
-		//double number=0; //3: CANT HAVE A VAR WITH THE SAME NAME ASA METHOD PARAM 
+		//double number=0; //3: CANT HAVE A VAR WITH THE SAME NAME AS A METHOD PARAM 
 		System.out.println("name of phone is: " + name);
 		System.out.println("weight of phone is: " + weight);
 		
@@ -29,6 +29,18 @@ public class Phone {
 		//Any references in here to 'name' will refer to the var here. To target the global name var, use 'this' +++++++++++++++++++++++++++ 
 		System.out.println("name of local name var is: " + name);
 		System.out.println("name of global name var is: " + this.name);
+		
+		System.out.println("colour of phone is: " + colour);
+	
+		
+		{
+			//You can't create a static var inside a method, but you can shadow a static.  
+			String colour = "blue"; //creating a colour var which shadows the static var, and is local ONLY to these brackets
+			System.out.println("local colour is: " + colour);
+		}
+		
+		System.out.println("colour of phone is: " + colour);
+		
 	}
 	
 
