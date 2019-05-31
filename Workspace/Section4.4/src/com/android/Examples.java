2a3f88ec4ea3286e3cabc25ba25e83aeeefc7f7f
jasonthorne <jasonthorne@live.ie>
2019-05-31 12:20:00 +0100

    EOD push

--- Workspace/Section4.4/src/com/android/Examples.java
+++ Workspace/Section4.4/src/com/android/Examples.java
@@ -40,6 +40,60 @@ public class Examples {
 		//printing the elements of an arraylist. (the object's toString method has been overridden to print these)
 		System.out.println(names);
 		
+		/*
+		 * An arrayList can ONLY take objects. It cannot take primitives: 
+		 * ArrayList<string>names = new ArrayList<>(); //wont Compile.
+		 * 
+		 * It has to be Wrapper Classes:
+		 */
+		
+		ArrayList<Integer>integerList = new ArrayList<>();
+		ArrayList<Double>doubleList = new ArrayList<>();
+		ArrayList<Character>characterList = new ArrayList<>();
+		
+		//this int is autoboxed into an Intger:
+		integerList.add(1);
+		
+		//adding other values to lists:
+		integerList.add(new Integer(4));
+		integerList.add(100);
+		integerList.add(1000);
+		
+		//this double is autoboxed into a Double:
+		doubleList.add(2.34);
+		doubleList.add(new Double(45.78));
+		doubleList.add(3.99);
+		doubleList.add(123.77);
+		
+		//'e' is autoboxed into a Character wrapper obj:
+		characterList.add('e');
+		characterList.add(new Character('a'));
+		characterList.add('b');
+		characterList.add('!');
+		
+		
+		//printing arrayLists:
+		System.out.println(integerList);
+		System.out.println(doubleList);
+		System.out.println(characterList);
+		
+		
+		//spinnig through list;
+		for(Integer i:integerList) {
+			System.out.println("number is " + i);
+		}
+		
+		
+		//for arrays we use the var length to hget the length of the array. For arrayLists we use the method size() to the size of the arraylist.
+		
+		//arraylist numbering starts at 0
+		System.out.println("\nfor loop:");
+		for(int i=0;i<names.size();i++) {
+			System.out.println(names.get(i));
+		}
+		
+		
+		
 		
 	}
 }
