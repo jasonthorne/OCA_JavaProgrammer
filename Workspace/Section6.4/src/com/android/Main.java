bd1864babba9cc1553a1b98a7da981e8e1adde0b
jasonthorne <jasonthorne@live.ie>
2019-06-27 12:30:08 +0100

    6.4 - 3

--- Workspace/Section6.4/src/com/android/Main.java
+++ Workspace/Section6.4/src/com/android/Main.java
@@ -60,7 +60,108 @@ public class Main {
 		}
 		*/
 		
-
+		
+		//+++++++++++++++++++++++++++++++++++++++++++++++++
+		
+		//INSTANCE OF 
+		
+		Dog spot = new Dog();
+		Animal angela = new Animal();
+		Pig pippa = new Pig();
+		
+		/*
+		 * We have a program where multiple different types of animals are prodced.
+		 * We know spot and angela and pippa are animals.
+		 * As an example we will say with this program that we dont know what type of Animals spot and pippa are.
+		 * 
+		 * instanceOf returns a boolean
+		 * true if it is a particular object
+		 * false if it is not a particular object
+		 */
+		
+		System.out.println(spot instanceof Dog);
+		System.out.println(pippa instanceof Pig);
+		System.out.println(pippa instanceof Animal);
+		
+		
+		//false, as instanceOf only checks the OBJECT not the reference:
+		System.out.println(angela instanceof Pig); 
+		
+		//true, as instanceOf only checks the OBJECT not the reference:
+		Animal animalPig = new Pig();
+		System.out.println(animalPig instanceof Pig); 
+		
+		/*If we have an Animal reference to an Animal object and we go:
+		 * angela instanceof Pig
+		 * 
+		 *  however if we change angela to angela = new Pig()
+		 *  then angela instanceof Pig will return true.
+		 *  it only checks the OBJECT not the reference.
+		 *  
+		 *  System.out.println(spot instanceof Animal); 
+		 * 
+		 */
+		
+		
+		//-------------------------------------------
+		
+		//GETCLASS
+		
+		/*
+		 * Returns a string which is the fully quaified name of the class, which inthis case is com.android.Dog
+		 */
+		
+		System.out.println("spot's class is " + spot.getClass());
+		System.out.println("spot's class is " + spot.getClass().getSimpleName()); //just gets the class name
+		
+		
+		
+		Animal[]farm = {new Cow(), new Sheep(), new Pig(), new Chicken(), new Fresian(), new Sheep(), new Animal()};
+		
+		int sheepCounter = 0;
+		int cowCounter = 0;
+		
+		for(Animal a:farm) {
+			
+			boolean sheepCheck = a instanceof Sheep; //holds if a is a reference to a Sheep object
+			boolean cowCheck = a instanceof Cow;
+			
+			if (sheepCheck) {
+				sheepCounter++;
+			}
+			if (cowCheck) {
+				cowCounter++;
+			}
+		}
+		
+		System.out.println("\nyou have " + sheepCounter + " sheep.");
+		System.out.println("you have " + cowCounter + " cows.");
+		
+		//---------------------------
+		
+		Animal andy = new Animal();
+		Cow daisy = new Cow();
+		
+		
+		//TRYING TO CHANGE THE REFERENCE NOT THE OBJECT +++++++++++++++++++++++++++++++++++++++++++++++++
+		/*
+		 * Below wont compile a andy is an Animal object, which is a superclass object,
+		 * and casting to a Cow only changes the reference, 
+		 * so then you would have a subclass Cow reference to a superclass object, 
+		 * which you cant have and you get a class cast exception. 
+		 */
+		//andy=(Cow)andy;
+		
+		//andy is an animal reference to an animal object
+		
+		/*
+		 * This does NOT change daisy to an Animal object, this only means that the below object is an Animal reference to a Cow obj.
+		 * casting only changes the reference. 
+		 */
+		andy = (Animal)daisy;
+		
+		
+		
 	}
 
 }
