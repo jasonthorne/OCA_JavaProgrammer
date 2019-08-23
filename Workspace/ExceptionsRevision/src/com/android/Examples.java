2a6073aca9aa369e13ccede4196586057538c4ed
jasonthorne <jasonthorne@live.ie>
2019-08-23 12:14:25 +0100

    Exceptions revision ex5

--- Workspace/ExceptionsRevision/src/com/android/Examples.java
+++ Workspace/ExceptionsRevision/src/com/android/Examples.java
@@ -101,6 +101,26 @@ public class Examples {
 	
 	public static void ex3() /*throws Exception*/ {
 		
+		File myFile = new File("c:/Test2.txt");
+		
+		try {
+			
+			/*
+			 * If exception is generated here, then any code in the try block wont be run (System.out.println)
+			 */
+			myFile.createNewFile();
+			System.out.println("no exception generated");
+		}
+		catch(Exception e) {
+			System.out.println("No dice, punk! Exception caught is: " + e);
+			
+		}
+		finally {
+			System.out.println("finally is run");
+		}
+		
+		
+		System.out.println("this code runs no mattaer what");
 		
 		/*
 		 * There are two broad types of Exception: Checked & Runtime (unchecked)
@@ -109,5 +129,102 @@ public class Examples {
 		 * You should NOT handle errors. Always something outside of coder's control (eg a hardware problem).
 		 */
 	}
+	
+	
+	public static void ex4() /*throws Exception*/ {
+	
+		
+		System.out.println("runtime exceptions");
+		
+		int[]nums = {45,78,999};
+		
+		//System.out.println(nums[5]); //this will compile as this causes a runtime exception, which doesnt need to be in a try block, as it sohuld be fixed in code.
+		
+		
+		try {
+			///System.out.println(nums[5]);
+			
+			String nullStr = null;
+			System.out.println(nullStr.length()); //causes a null pointer exception
+		}
+		
+		
+		//SUBCLASSES FIRST, THEN SUPERCLASSES:
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException: " + e.getClass().getSimpleName());
+			
+		}
+		
+		catch(IndexOutOfBoundsException e) {
+			System.out.println("IndexOutOfBoundsException: " + e.getClass().getSimpleName());
+			
+		}
+		
+		
+		catch(RuntimeException e) {
+			System.out.println("RuntimeException: " + e.getClass().getSimpleName());
+			
+		}
+		
+
+		catch(Exception e) {
+			System.out.println("Exception: " + e.getClass().getSimpleName());
+			
+		}
+		
+		
+		finally {
+			System.out.println("finally is run");
+		}
+		
+		System.out.println("this code runs no mattaer what");
+		
+		
+	}
+	
+	
+	//++++++++++++++++++++++++++++++++++THROWS: +++++++++++++++++++++++++++++++++++++++++++++
+	
+	
+	public static void ex5(){
+		
+		try {
+			//throwMe1();
+			throwMe2();
+		}
+		catch(Exception e) {
+			System.out.println("Exception: " + e);
+			
+		}
+		
+		
+		try {
+			noThrow(); //is in try because method signature has "throws Exception"
+		}
+		catch(Exception e) {
+			System.out.println("Exception: " + e);
+			
+		}
+	}
+		
+	static void throwMe1() throws Exception{
+		System.out.println("throwMe1 method");
+		File myFile = new File("c:/Test2.txt");
+		myFile.createNewFile();
+	}
+	
+	
+	static void throwMe2() throws Exception{
+		System.out.println("throwMe2 method");
+		File myFile = new File("Test3.txt");
+		myFile.createNewFile();
+	}
+	
+	
+	static void noThrow() throws Exception{ 
+		System.out.println("noThrow called"); //DOESNT throw but still needs to be in a try block because method signature is marked to throw (so it COULD).
+	}
+	
+	
 
 }
--- Workspace/ExceptionsRevision/src/com/android/Main.java
+++ Workspace/ExceptionsRevision/src/com/android/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Examples.ex1();
+		//Examples.ex1();
+		
+		//Examples.ex2();
+		
+		//Examples.ex3();
+		
+		//Examples.ex4();
+		
+		Examples.ex5();
 	
 
 	}
