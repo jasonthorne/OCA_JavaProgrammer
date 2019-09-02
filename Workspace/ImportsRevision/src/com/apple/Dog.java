b7bcb527c51054165789a4638062231789f9e507
jasonthorne <jasonthorne@live.ie>
2019-09-02 15:21:19 +0100

    ImportsRevision - 3

--- Workspace/ImportsRevision/src/com/apple/Dog.java
+++ Workspace/ImportsRevision/src/com/apple/Dog.java
@@ -2,7 +2,48 @@ package com.apple;
 
 public class Dog {
 
+int age;
 	
+	protected String name;
+	
+	public double height;
+	
+	private double weight;
+	
+	Dog(int age){
+		this.age=age;
+	}
+	
+	protected Dog() {
+		System.out.println("protected Dog constructor called");
+	}
+	
+	public Dog(String name) {
+		this.name=name;
+		System.out.println("public Dog constructor called");
+	}
+	
+	private Dog(String name, double weight) {
+		this.name=name;
+		this.weight = weight;
+		System.out.println("private Dog constructor called");
+	}
+	
+	protected void eat() {
+		System.out.println("protected Dog eat called");
+	}
+	
+	public void drink() {
+		System.out.println("public Dog drink called");
+	}
+	
+	void run() {
+		System.out.println("default (package level) Dog run called");
+	}
+	
+	private void bark() {
+		System.out.println("private Dog bark called");
+	}
 	
 	
 }
--- Workspace/ImportsRevision/src/com/windows/Collie.java
+++ Workspace/ImportsRevision/src/com/windows/Collie.java
@@ -3,5 +3,30 @@ package com.windows;
 import com.apple.Dog; //This imports all of the public and protected members of the Dog class (as this is a SUBCLASS of Dog).
 
 public class Collie extends Dog {
+	
+	//Dog myDog= new Dog();
+	
+	Collie(){
+		/*this is calling the constructor in the super class that takes no args
+		the dog class is the super class of the Collie class and there is a construvtor in the dog class that has the following signiature:
+		protected Dog()
+		*/
+		super();
+	}
+	
+
+	Collie(String name){
+		/*
+		 * This is calling the construvtor in the super class that takes a String
+		 * the Dog class is the super of the collie class and there is a constructor in the Dog class that has the following sig:
+		 * public Dog(String name)
+		 */
+		super(name);
+		eat(); //protected method
+		drink(); //public method
+	}
+	
+	
+	
 
 }
