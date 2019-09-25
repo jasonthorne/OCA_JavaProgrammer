f24057f80ce7e93efb7d730aab30f1ba4ca0e72d
jasonthorne <jasonthorne@live.ie>
2019-09-25 12:31:12 +0100

    quiz code2

--- Workspace/CodeForQuiz/src/Main.java
+++ Workspace/CodeForQuiz/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 		
 		/*
-		 QUESTION 440
+		 Q 440
 	
 		public class Test{
 			public static final int MIN = 1;
@@ -29,7 +29,7 @@ public class Main {
 	/*
 	 
 	 
-	 QUESTION 444
+	 Q 444
 	 
 	 public static void main (String [] args){
 	 	String myStr = "Hello World";
@@ -41,20 +41,140 @@ public class Main {
 	 */
 	
 	
-	/*
-	 
-	 QUESTION 445
+	
+	 /*
+	 //Q 445
+	
+	public static void main (String [] args) {
+		int [] stack = {10,20,30};
+		int size = 3;
+		int idx = 0;
+		//line n1 
+		System.out.println("The top element: " + stack[idx]);
+	}
 	 
 	 do{
 	 	idx++;
-	 }while (idx >=)
+	 }while (idx >=size);
 	 
+	 while (idx < size){
+		 idx++;
+	 }
 	 
+	 do{
+	 	idx++;
+	 }while (idx <size -1);
+	 
+	 do{
+	 	idx++;
+	 }while (idx <=size);
+	 
+	 while (idx <= size-1){
+		 idx++
+	 }
 	 */
+	 
 	
+	/*
+	 
+	//Q 448
 	
+	public class App{
+		static int count;
+		public static void displayMsg() {
+			count++; //line n1
+			System.out.println("Welcome" + "Visist Count: " + count); //line n2
+		}
+		public static void main (String[] args) {
+			App.displayMsg(); //line n3
+			App.displayMsg(); //line n4
+		}
+	}
+	
+	*/
+	
+	/*
+	
+	//Q 449
+	
+	public static void main (String[] args) {
+		System.out.println("Result A" + 0 + 1);
+		System.out.println("Result B" + (1) + (2));
+	}
 		
+	*/
 
+	
+	/*
+	
+	//Q 450
+	
+	class Test{
+		public static void main (String[] args) {
+			int numbers [] = {12, 13, 42, 32, 15, 156, 23, 51, 12};
+			int max = findMax(numbers);
+		}
+		
+		//line n1 {
+			int max = 0;
+			// code goes here
+			return max;
+		}
+	}
+
+	*/
+	
+	
+	/*
+	
+	//Q 451`
+	
+	class A {
+		public void test() {
+			System.out.println("A");
+		}
+	}
+	
+	class B extends A{
+		public void test() {
+			System.out.println("B");
+		}
+	}
+	
+	public class C extends A{
+		public void test() {
+			System.out.println("C");
+		}
+		
+		
+		public static void main (String[] args) {
+			A b1 = new A ();
+			A b2 = new C ();
+			b1 = (A) b2;
+			A b3 = (B) b2; //line n1
+			A b3 = (B) b2; //line n2
+			b1.test ();
+			b3.test ();
+		}
+	}
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
