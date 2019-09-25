41bd46dc658727f0ae1a359e4587b99fa0973f95
jasonthorne <jasonthorne@live.ie>
2019-09-25 16:03:57 +0100

    quiz code5

--- Workspace/CodeForQuiz/src/Main.java
+++ Workspace/CodeForQuiz/src/Main.java
@@ -226,8 +226,22 @@ public class Main {
 	*/
 
 	
+	/*
+	//Q 467
 	
+	public static void main (String[] args) {
+		int iVar = 100;
+		float fVar = 100.100f;
+		double dVar = 123;
+		iVar = fVar;
+		fVar = iVar;
+		dVar = fVar;
+		fVar = dVar;
+		dVar = iVar;
+		iVar = dVar;
+	}
 	
+	*/
 	
 	
 	
