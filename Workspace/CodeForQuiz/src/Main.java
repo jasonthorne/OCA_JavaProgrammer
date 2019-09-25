0c852a2aed5e83c35e4aa61ad89bc94481c6004a
jasonthorne <jasonthorne@live.ie>
2019-09-25 14:16:37 +0100

    quiz code3

--- Workspace/CodeForQuiz/src/Main.java
+++ Workspace/CodeForQuiz/src/Main.java
@@ -161,6 +161,43 @@ public class Main {
 	*/
 	
 	
+	/*
+	
+	//Q 455
+	
+	
+	class Animal {
+		String type = "Canine";
+		int maxSpeed = 60;
+		
+		Animal(){}
+		
+		Animal (String type, int maxSpeed){
+			this.type = type;
+			this.maxSpeed = maxSpeed;
+			
+		}
+	}
+	
+	class WildAnimal extends Animal{
+		String bounds;
+		
+		WildAnimal (String bounds){
+			//line n1
+		}
+		
+		WildAnimal (String type, int maxSpeed){
+			//line n2
+		}
+	}
+	
+	
+	WildAnimal wolf = new WildAnimal ("Long");
+	WildAnimal tiger = new WildAnimal ("Feline", 80, "Short");
+	System.out.println(wolf.type + " " + wolf.maxSpeed + " " + wolf.bounds);
+	System.out.println(tiger.type + " " + tiger.maxSpeed + " " + tiger.bounds);
+	
+	*/
 	
 	
 	
