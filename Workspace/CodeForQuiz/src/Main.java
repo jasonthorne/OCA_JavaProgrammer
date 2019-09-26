db98217763d12176180311aa474d333ca0cec052
jasonthorne <jasonthorne@live.ie>
2019-09-26 12:19:55 +0100

    quiz code6

--- Workspace/CodeForQuiz/src/Main.java
+++ Workspace/CodeForQuiz/src/Main.java
@@ -243,6 +243,28 @@ public class Main {
 	
 	*/
 	
+	/*
+	//Q 472
+	
+	public class CheckingAccount {
+		public int amount;
+		public CheckingAccount(int amount) {
+			this.amount = amount;
+		}
+		public int getAmount() {
+			return amount;
+		}
+		public void changeAmount(int x) {
+			amount += x;
+		}
+	}
+	
+	public static void main(String [] args) {
+		CheckingAccount acct = new CheckingAccount((int)(Math.random(0*1000)));
+		//line n1
+		System.out.println(acct.getAmount());
+	}
+	*/
 	
 	
 	
