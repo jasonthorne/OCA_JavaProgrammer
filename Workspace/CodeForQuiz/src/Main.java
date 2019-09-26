eb5a7a77df60e5c3523f42d8e471e67a4a02cd03
jasonthorne <jasonthorne@live.ie>
2019-09-26 15:05:04 +0100

    quiz code7

--- Workspace/CodeForQuiz/src/Main.java
+++ Workspace/CodeForQuiz/src/Main.java
@@ -266,6 +266,23 @@ public class Main {
 	}
 	*/
 	
+	/*
+	//Q487
+	
+	public class Test {
+		public static void main(String[] args) {
+			int x = 1;
+			int y = 0;
+			if(x++ > ++y) {
+				System.out.println("Hello ");
+			}else {
+				System.out.println("Welcome ");
+			}
+			System.out.println("Log " + x + ":" + y);
+		}
+	}
+	*/
+	
 	
 	
 	
