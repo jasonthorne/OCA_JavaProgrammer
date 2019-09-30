c99260007462d1677afee2d57013d8095ba2fe29
jasonthorne <jasonthorne@live.ie>
2019-09-30 15:57:31 +0100

    quizCode11

--- Workspace/CodeForQuiz/src/Main.java
+++ Workspace/CodeForQuiz/src/Main.java
@@ -427,4 +427,27 @@ public class Main {
 		
 	}
 	*/
+	
+	/*
+	public static void main(String [] args){
+	String[][] chs = new String[2][];
+	  chs[0] = new String[2];
+	  chs[1] = new String[5];
+	  int i = 97;
+	  
+	  for (int a = 0; a < chs.length(); a++){
+		  for(int b = 0; b < chs.length(); b++){
+			  chs[a][b] = ""+i;
+			  i++;
+		  }
+	  }
+	  for (String[] ca : chs){
+		  for (String c : ca){
+			  System.out.println(c + "");
+		  }
+		  System.out.println();
+	  }
+	}
+	*/
+
 }
